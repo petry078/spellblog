f73d83fa8f01cfdc12004e180609349b6de7db69
petry078 <petry078@users.noreply.github.com>
2025-01-23 18:14:12 +0000

    deploy: 2fdb8d476a9ab9c93cadc30b98a392bcdc712830

---
+++ assets/markdown-cheat-sheet.html.f36bef96.js
@@ -0,0 +1,31 @@
+import{_ as e,o as i,c as n,e as t}from"./app.ef2b660b.js";const d={},l=t(`<h1 id="markdown-cheat-sheet" tabindex="-1"><a class="header-anchor" href="#markdown-cheat-sheet" aria-hidden="true">#</a> Markdown cheat sheet</h1><div class="language-text ext-text line-numbers-mode"><pre class="language-text"><code>| Markup syntax    | Name                            | Description &amp; Examples            |
+|------------------|---------------------------------|-----------------------------------|
+| #                | Heading 1 &lt;h1&gt;                  | # H1                              |
+| ##               | Heading 2 &lt;h2&gt;                  | ## H2                             |
+| ###              | Heading 3 &lt;h3&gt;                  | ### H3                            |   
+| ####             | Heading 4 &lt;h4&gt;                  | #### H4                           | 
+| #####            | Heading 5 &lt;h5&gt;                  | ##### H5                          |  
+| ######           | Heading 6 &lt;h6&gt;                  | ###### H6                         | 
+| **bold**         | Bold                            | **Bold** example                  |
+| *italic*         | Italic                          | *Italic* example                  | 
+| ---              | Line &lt;hr&gt;                       | Line break (*** ___ )             |
+| &gt;                | Quote/Comment                   | &gt; Comment styled line             |
+| *                | Unordered list item             | * List item (- + are alternatives)|
+| 1.               | Number ordered list item        | 1. Just like this!                |
+| a.               | Character ordered list item     | a. Character styled list          |
+| - [ ] text       | Unmarked to-do list item        | Create to-do lists                |
+| - [x] text       | Checkd to-do list item          | This one is a checked [\u2714] box     |
+| \`\`\`code\`\`\`       | Code                            | \`\`\`Code styled line/block\`\`\`      | 
+| [text](URL)      | Hyperlink                       | [Download here!](URL)             |
+| ![alt text](URL) | Image                           | ![altText](URL)                   |
+| \\                | Disable Markdown style          | Use before \`\`\` * # - + ! |        |
+|------------------|---------------------------------|-----------------------------------|
+|                                                                                        |
+|                                 text-align on tables                                   |
+|                                                                                        |
+|               | text-align: left | text-align: center | text-align: right |            |
+|               |:-----------------|:------------------:|------------------:|            |
+|               | text             |        text        |              text |            |
+|                                                                                        |
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><div class="wisdom"><img class="wisdony" src="https://upload.wikimedia.org/wikipedia/commons/d/d7/Cups05.jpg" alt=""></div>`,3),s=[l];function a(r,c){return i(),n("div",null,s)}var m=e(d,[["render",a],["__file","markdown-cheat-sheet.html.vue"]]);export{m as default};
